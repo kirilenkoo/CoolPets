4cdef182a287674f8be13ecb8a70912b4933e15d
Unknown <kirilenko1988@sina.com>
2018-10-30 18:31:20 +0800

    init StateImageView

---
+++ app/src/main/java/cn/kirilenkoo/www/coolpets/ui/view/StateImageView.kt
@@ -0,0 +1,13 @@
+package cn.kirilenkoo.www.coolpets.ui.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+
+class StateImageView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr)  {
+    
+}
